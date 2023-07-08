fd08b63c146691587f776b17b166d92074052501
Alessandro Ceserani <aceserani@gmail.com>
2023-07-08 01:20:10 -0500

    refactor: Expose subcommands dynamically in main module

--- lua/hbac/init.lua
+++ lua/hbac/init.lua
@@ -1,15 +1,11 @@
 local command = require("hbac.command")
 local subcommands = require("hbac.command.subcommands")
 
-return {
+local M = {
 	setup = require("hbac.setup").setup,
 	cmd = command.vim_cmd_func,
-	close_unpinned = subcommands.close_unpinned,
-	pin_picker = subcommands.pin_picker,
-	pin_all = subcommands.pin_all,
-	unpin_all = subcommands.unpin_all,
-	store_pinned_bufs = subcommands.store_pinned_bufs,
-	storage_picker = subcommands.storage_picker,
-	toggle_autoclose = subcommands.toggle_autoclose,
-	toggle_pin = subcommands.toggle_pin,
 }
+
+M = vim.tbl_extend("error", M, subcommands)
+
+return M
